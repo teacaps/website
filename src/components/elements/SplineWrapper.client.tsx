783042752a12f12cbd8f30b16be9af8c449bc453
futurGH <me@futuristick.ca>
2022-08-29 22:43:02 -0400

    Prevent sub-pixel canvas height discrepancy from destroying the universe

--- src/components/elements/SplineWrapper.client.tsx
+++ src/components/elements/SplineWrapper.client.tsx
@@ -8,11 +8,12 @@ export function Spline(props: Parameters<typeof RealSpline>[0]) {
 			style={{ display: "block" }}
 			{...props}
 			onLoad={({ canvas }: Application) => {
-				canvas.style.width = ``;
-				canvas.style.height = ``;
+				canvas.style.width = "";
+				canvas.style.height = "";
 				// Set the canvas width/height on resize or else events won't trigger.
 				const ro = new ResizeObserver(() => {
 					const rect = canvas.getBoundingClientRect();
+					if (Math.abs(rect.height - canvas.height) < 10) return;
 					canvas.width = rect.width;
 					canvas.height = rect.height;
 				});

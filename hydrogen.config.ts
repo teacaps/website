15f94cac65de07d6888e740408c75816139cb4ae
futurGH <me@futuristick.ca>
2022-09-10 10:52:42 -0400

    I did that actually

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@shopify/hydrogen/config";
 
 declare const Oxygen: any;
 
-// Swap out for real config at a later date
 export default defineConfig({
 	shopify: {
 		storeDomain: `${Oxygen?.env?.STOREFRONT_NAME!}.myshopify.com`,

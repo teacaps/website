bb29abe9e2d7731c847ae77f275fc3baf55ea91e
futurGH <me@futuristick.ca>
2022-09-24 18:40:54 -0400

    Suddenly Spline works without any black magic or arcane trickery? I am equal parts terrified and grateful

--- src/components/elements/SplineWrapper.client.tsx
+++ src/components/elements/SplineWrapper.client.tsx
@@ -1,32 +1,21 @@
 import RealSpline from "@splinetool/react-spline";
 import type { Application } from "@splinetool/runtime";
+import type { CSSProperties } from "react";
 
 export function Spline(props: Parameters<typeof RealSpline>[0]) {
+	const style: CSSProperties = {
+		// Override the default `display: "none"` inline style during loading so there's no flicker.
+		display: "block",
+		pointerEvents: "none",
+	};
 	return (
 		<RealSpline
-			// Override the default `display: "none"` inline style during loading so there's no flicker.
-			style={{ display: "block" }}
+			style={style}
 			{...props}
 			onLoad={({ canvas }: Application) => {
 				canvas.style.width = "";
 				canvas.style.height = "";
-				// Set the canvas width/height on resize or else events won't trigger.
-				window.addEventListener("resize", () => adjustCanvasSize(canvas));
-				const emptyDataURL = canvas.toDataURL();
-				const waitForContent = setInterval(() => {
-					if (emptyDataURL !== canvas.toDataURL()) {
-						clearInterval(waitForContent);
-						adjustCanvasSize(canvas);
-					}
-				}, 500);
 			}}
 		/>
 	);
-}
-
-function adjustCanvasSize(canvas: HTMLCanvasElement) {
-	const { devicePixelRatio: ratio } = window;
-	const { width, height } = canvas.getBoundingClientRect();
-	canvas.width = width * ratio;
-	canvas.height = height * ratio;
 }

19b39ec6088a82f6dc8ab2bbf77d3d16c2765052
futurGH <me@futuristick.ca>
2022-09-29 12:35:51 -0400

    Try using Vercel's Hydrogen zero-config deployment preset

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -1,12 +1,12 @@
 /// <reference path="global.d.ts" />
 import { defineConfig } from "@shopify/hydrogen/config";
 
-Oxygen ??= { env: (process.env as Record<string, string>) || import.meta.env };
+globalThis.Oxygen ??= { env: (process.env as Record<string, string>) || import.meta.env };
 
 export default defineConfig({
 	shopify: {
-		storeDomain: `${Oxygen.env.STOREFRONT_NAME}.myshopify.com`,
-		storefrontToken: Oxygen.env.STOREFRONT_TOKEN,
-		storefrontApiVersion: Oxygen.env.STOREFRONT_API_VERSION,
+		storeDomain: `${globalThis.Oxygen.env.STOREFRONT_NAME}.myshopify.com`,
+		storefrontToken: globalThis.Oxygen.env.STOREFRONT_TOKEN,
+		storefrontApiVersion: globalThis.Oxygen.env.STOREFRONT_API_VERSION,
 	},
 });

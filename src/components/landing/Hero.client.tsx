c0e5d4e849e9b6d2a2cad4608739d8c61224641f
futurGH <me@futuristick.ca>
2022-10-01 00:26:19 -0400

    Use a video for the hero section teacup
    Would have been absolutely impossible without John Morton of
    supergeekery.com. Learned far more about video encodings than
    any sane person should ever have to know.

--- src/components/landing/Hero.client.tsx
+++ src/components/landing/Hero.client.tsx
@@ -15,13 +15,19 @@ export function Hero() {
 				</ButtonLink>
 			</Container>
 			<video
-				src="/assets/hero-teacup.webm"
 				className="md:[60vw] ml-auto mb-16 mt-10 w-[75vw] sm:mb-24 sm:mt-16 lg:-mt-16 lg:w-[45vw] xl:-mt-24 2xl:-mt-36 2xl:w-[40vw]"
 				autoPlay
 				loop
 				muted
-				playsInline
-			/>
+				playsInline>
+				{/* https://supergeekery.com/blog/transparent-video-in-chrome-edge-firefox-and-safari-circa-2022 */}
+				{/* PNG sequence to ProRes 4444 XQ in After Effects, then convert to H.265 in Shutter Encoder */}
+				{/* Really only used to support Safari */}
+				<source src="/assets/hero-teacup.mp4" type="video/mp4;codecs=hvc1" />
+				{/* PNG sequence to webm with libvpx-vp9 in ffmpeg */}
+				{/* Generally universal support aside from Safari */}
+				<source src="/assets/hero-teacup.webm" type="video/webm" />
+			</video>
 		</>
 	);
 }

3fb3a24b1349e3a7d54a18bd3c00b8512c6e34d6
Curtis Draheim <38147998+amDizzi@users.noreply.github.com>
2022-10-10 20:09:28 -0700

    Update ShippingNotice.tsx
    Updated shipping notice

--- src/components/elements/header/ShippingNotice.tsx
+++ src/components/elements/header/ShippingNotice.tsx
@@ -2,7 +2,7 @@ export function ShippingNotice() {
 	return (
 		<div className="flex h-auto w-full content-center justify-center bg-matcha py-2 px-4 text-sm sm:px-0 sm:text-base">
 			<span className="text-center font-medium text-grain leading-6">
-				Free domestic shipping on orders over $100
+				Free domestic shipping for Sencha
 			</span>
 		</div>
 	);

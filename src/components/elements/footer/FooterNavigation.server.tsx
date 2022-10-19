07fc344b00056b08e632b997445fab1f4fdf1e6c
futurGH <me@futuristick.ca>
2022-10-19 16:14:27 -0400

    Add blog to footer

--- src/components/elements/footer/FooterNavigation.server.tsx
+++ src/components/elements/footer/FooterNavigation.server.tsx
@@ -2,16 +2,11 @@ import { Link } from "@shopify/hydrogen";
 
 export function FooterNavigation() {
 	return (
-		<div className="flex flex-col space-y-4 text-grain leading-6">
-			<Link to="/contact-us" className="hover:font-bold">
-				Contact
-			</Link>
-			<Link to="/shipping-returns" className="hover:font-bold">
-				Shipping & Returns
-			</Link>
-			<Link to="/tos-privacy" className="hover:font-bold">
-				Terms & Privacy
-			</Link>
+		<div className="only-of-type:a flex flex-col space-y-4 text-grain leading-6 hover:[&>a]:font-bold">
+			<Link to="/blog">Blog</Link>
+			<Link to="/contact-us">Contact</Link>
+			<Link to="/shipping-returns">Shipping & Returns</Link>
+			<Link to="/tos-privacy">Terms & Privacy</Link>
 		</div>
 	);
 }

9237dee17fa244a980a860535711819461282b9f
futurGH <me@futuristick.ca>
2022-12-09 13:10:45 -0500

    Add BSB to landing page

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -21,6 +21,13 @@ export default function Landing() {
 			<Hero />
 			<Welcome />
 			<ProductSection
+				name="Brown Sugar Boba"
+				description="Inspired by the delicious Brown Sugar Boba drink, created in Taiwan and loved around the world. Version two of Brown Sugar Boba features updated legends and revised novelty kitting."
+				url="/products/brown-sugar-boba"
+				align="left"
+				imageDimensions={{ width: 1000, height: 568 }}
+			/>
+			<ProductSection
 				name="Ritual"
 				description="Crisp mornings and quiet evenings. Nothing brings comfort like habits and routines. Ritual encompasses the feeling of a routine well-kept. Choose from a lighter morning variant, or a darker  evening variant."
 				url="/products/ritual"

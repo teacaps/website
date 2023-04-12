24f1a3cbb277a7c3304f5d82cdcbf0e8512eb928
futurGH <me@futuristick.ca>
2023-04-12 09:57:35 -0400

    Tweak config

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -1,20 +1,21 @@
-import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+/// <reference path="global.d.ts" />
+import { CookieSessionStorage, defineConfig } from "@shopify/hydrogen/config";
 
+globalThis.Oxygen ??= { env: Oxygen?.env || (process.env as Record<string, string>) || import.meta.env };
 export default defineConfig({
-  shopify: {
-    defaultCountryCode: 'US',
-    defaultLanguageCode: 'EN',
-    storeDomain: Oxygen.env.PUBLIC_STORE_DOMAIN,
-    storefrontToken: Oxygen.env.PUBLIC_STOREFRONT_API_TOKEN,
-    privateStorefrontToken: Oxygen.env.PRIVATE_STOREFRONT_API_TOKEN,
-    storefrontApiVersion: '2022-07',
-    storefrontId: Oxygen.env.PUBLIC_STOREFRONT_ID,
-  },
-  session: CookieSessionStorage('__session', {
-    path: '/',
-    httpOnly: true,
-    secure: import.meta.env.PROD,
-    sameSite: 'Strict',
-    maxAge: 60 * 60 * 24 * 30,
-  }),
+	shopify: {
+		defaultCountryCode: "US",
+		defaultLanguageCode: "EN",
+		storeDomain: "checkout.teacaps.studio",
+		storefrontToken: globalThis.Oxygen.env.PUBLIC_STOREFRONT_API_TOKEN,
+		storefrontApiVersion: globalThis.Oxygen.env.PRIVATE_STOREFRONT_API_TOKEN,
+	},
+	serverErrorPage: "./src/global/NotFound.server.tsx",
+	session: CookieSessionStorage("__session", {
+		path: "/",
+		httpOnly: true,
+		secure: import.meta.env.PROD,
+		sameSite: "Strict",
+		maxAge: 60 * 60 * 24 * 30,
+	}),
 });

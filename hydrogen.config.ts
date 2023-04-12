392385109429c2257d2a36b16869808cf9bbfffb
futurGH <me@futuristick.ca>
2023-04-12 10:00:51 -0400

    Revert "Add Oxygen deployment workflow file and update Shopify configuration"

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -4,17 +4,15 @@ import { CookieSessionStorage, defineConfig } from "@shopify/hydrogen/config";
 globalThis.Oxygen ??= { env: Oxygen?.env || (process.env as Record<string, string>) || import.meta.env };
 export default defineConfig({
 	shopify: {
-		defaultCountryCode: "US",
-		defaultLanguageCode: "EN",
 		storeDomain: "checkout.teacaps.studio",
-		storefrontToken: globalThis.Oxygen.env.PUBLIC_STOREFRONT_API_TOKEN,
-		storefrontApiVersion: globalThis.Oxygen.env.PRIVATE_STOREFRONT_API_TOKEN,
+		storefrontToken: globalThis.Oxygen.env.STOREFRONT_TOKEN,
+		storefrontApiVersion: globalThis.Oxygen.env.STOREFRONT_API_VERSION,
 	},
 	serverErrorPage: "./src/global/NotFound.server.tsx",
 	session: CookieSessionStorage("__session", {
 		path: "/",
 		httpOnly: true,
-		secure: import.meta.env.PROD,
+		secure: process.env.NODE_ENV === "production",
 		sameSite: "Strict",
 		maxAge: 60 * 60 * 24 * 30,
 	}),

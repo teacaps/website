343720d0551e59c4f3303961a0e5bf371d0f1437
futurGH <me@futuristick.ca>
2023-05-02 17:02:18 -0400

    Add Parfait product section to landing

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -23,18 +23,18 @@ export default function Landing() {
 			<Hero />
 			<Welcome />
 			<ProductSection
+				name="Parfait"
+				description="Sweeten your setup with Parfait, a set inspired by our favorite desserts. Parfait draws from the creamy hues of the delicious frozen treat."
+				url="/products/parfait"
+				align="right"
+				imageDimensions={{ width: 1000, height: 510 }}
+			/>
+			<ProductSection
 				name="Brown Sugar Boba"
 				description="Inspired by the delicious Brown Sugar Boba drink, created in Taiwan and loved around the world. Version two of Brown Sugar Boba features updated legends and revised novelty kitting."
 				url="/products/brown-sugar-boba"
 				align="left"
 				imageDimensions={{ width: 1000, height: 568 }}
-			/>
-			<ProductSection
-				name="Ritual"
-				description="Crisp mornings and quiet evenings. Nothing brings comfort like habits and routines. Ritual encompasses the feeling of a routine well-kept. Choose from a lighter morning variant, or a darker  evening variant."
-				url="/products/ritual"
-				align="right"
-				imageDimensions={{ width: 1000, height: 582 }}
 			/>
 			{/* <Timeline /> */}
 		</Layout>

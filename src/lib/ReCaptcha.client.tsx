865994526844ef0705698bcf348d4b8f81cc99b9
futurGH <me@futuristick.ca>
2023-12-20 19:56:05 -0500

    Use Oxygen.env for recaptcha key

--- src/lib/ReCaptcha.client.tsx
+++ src/lib/ReCaptcha.client.tsx
@@ -1,10 +1,8 @@
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import type { ReactNode } from "react";
 
+const env: Record<string, string> = typeof Oxygen !== "undefined" && "env" in Oxygen ? Oxygen.env : import.meta.env;
+
 export function ReCaptcha({ children }: { children: ReactNode }) {
-	return (
-		<GoogleReCaptchaProvider reCaptchaKey={import.meta.env.PUBLIC_RECAPTCHA_SITE_KEY}>
-			{children}
-		</GoogleReCaptchaProvider>
-	);
+	return <GoogleReCaptchaProvider reCaptchaKey={env.PUBLIC_RECAPTCHA_SITE_KEY}>{children}</GoogleReCaptchaProvider>;
 }

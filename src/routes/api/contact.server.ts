2de5e56a6676d0d1cea05fef0dcb24aea1ac27af
futurGH <me@futuristick.ca>
2023-12-22 21:06:53 -0500

    6

--- src/routes/api/contact.server.ts
+++ src/routes/api/contact.server.ts
@@ -1,21 +1,18 @@
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 import type { HydrogenRequest } from "@shopify/hydrogen";
 
-const env: Record<string, string> = typeof Oxygen !== "undefined" && "env" in Oxygen ? Oxygen.env : import.meta.env;
-
 const client = new SESClient({
 	credentials: {
-		accessKeyId: env.SES_ACCESS_KEY_ID,
-		secretAccessKey: env.SES_ACCESS_KEY,
+		accessKeyId: Oxygen.env.SES_ACCESS_KEY_ID,
+		secretAccessKey: Oxygen.env.SES_ACCESS_KEY,
 	},
 	region: "us-east-1",
 });
 
 export async function api(request: HydrogenRequest) {
 	console.log(request);
-	console.log(JSON.stringify(env));
 	console.log("Oxygen");
-	console.log(Oxygen);
+	console.log(JSON.stringify(Oxygen));
 	if (request.method === "GET") return new Response(null, { status: 302, headers: { Location: "/contact-us" } });
 	if (request.method !== "POST") return new Response(null, { status: 405 });
 
@@ -37,7 +34,7 @@ export async function api(request: HydrogenRequest) {
 			method: "POST",
 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
 			body: new URLSearchParams({
-				secret: env.PRIVATE_RECAPTCHA_SECRET_KEY,
+				secret: Oxygen.env.PRIVATE_RECAPTCHA_SECRET_KEY,
 				response: recaptchaToken,
 			}),
 		});

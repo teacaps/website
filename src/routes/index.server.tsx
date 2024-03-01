ebbfd67a01ec7171f23b4bfafa147e6412014100
futurGH <me@futuristick.ca>
2024-02-29 19:54:15 -0500

    Update BSB illustration

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -23,20 +23,6 @@ export default function Landing() {
 			<Hero />
 			<Welcome />
 			<ProductSection
-				name="Rosewater"
-				description="Take a stroll through a tranquil garden with Rosewater. Soft pink tones bring out the best of a warm spring day."
-				url="/products/rosewater"
-				align="left"
-				imageDimensions={{ width: 1000, height: 438 }}
-			/>
-			<ProductSection
-				name="Parfait"
-				description="Sweeten your setup with Parfait, a set inspired by our favorite desserts. Parfait draws from the creamy hues of the delicious frozen treat."
-				url="/products/parfait"
-				align="right"
-				imageDimensions={{ width: 1000, height: 510 }}
-			/>
-			<ProductSection
 				name="Brown Sugar Boba"
 				description="Inspired by the delicious Brown Sugar Boba drink, created in Taiwan and loved around the world. Version two of Brown Sugar Boba features updated legends and revised novelty kitting."
 				url="/products/brown-sugar-boba"

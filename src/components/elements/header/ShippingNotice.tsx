b5f7c51f84c29bbff21f3315da62bb879def306d
futurGH <me@futuristick.ca>
2024-10-22 22:21:09 -0400

    Add Sprout 75 banner link

--- src/components/elements/header/ShippingNotice.tsx
+++ src/components/elements/header/ShippingNotice.tsx
@@ -1,9 +1,11 @@
 export function ShippingNotice() {
 	return (
-		<div className="flex h-auto w-full content-center justify-center bg-matcha py-2 px-4 text-sm sm:px-0 sm:text-base">
-			<span className="text-center font-medium text-grain leading-6">
-				Free domestic shipping on orders over $100
-			</span>
+		<div className="flex h-auto w-full content-center justify-center bg-matcha transition-colors text-sm hover:bg-[#E2794C] sm:text-base">
+			<a
+				className="w-full py-2 px-4 text-center font-medium text-grain leading-6 sm:px-0"
+				href="https://milktype.co/sprout75">
+				New from the Teacaps team: Sprout 75 is available for pre-order soon! →
+			</a>
 		</div>
 	);
 }
